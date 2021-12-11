bf26bf937b1330cd5ffbf005516364e044cc728e
elleom <el_leo_m@hotmail.com>
2021-12-11 20:54:34 +0100

    Initial commit

---
+++ src/main/java/com/example/sfrestapp/SfRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfrestapp/SfRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
